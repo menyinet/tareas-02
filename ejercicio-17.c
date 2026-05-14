1fd8cd8d20175b2a2f30a738ddba8e676635ca25
menyinet <menyi.app@gmail.com>
2026-05-14 17:53:53 +0200

    fin tarea 2

--- ejercicio-17.c
+++ ejercicio-17.c
@@ -10,11 +10,15 @@ int main(){
     //PROCESAMIENTO DATOS
     if(numero_introducido>0){
         //el numero es positivo y mostrar la tabla de multiplicar con un bucle
-        printf("El numero es positivo");
+        printf("El numero es positivo \n");
+        for(int i=1; i<=10;i++){
+            //bucle para mostrar la tabla de multiplicar del numero introducido
+            printf("El %d x %d = %d \n",i,numero_introducido, i*numero_introducido);
+        }
     } else if(numero_introducido<0) {
         //el numero es negativo y lo convierto a su valor absoluto  su valor en positivo
-        printf("El numero es negativo");
-
+        printf("El numero es negativo \n");
+        
     } else{
         printf("El numero es cero");// no hacemos nada solo indicarlo
     }
